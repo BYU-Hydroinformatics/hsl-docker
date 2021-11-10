0c6215cb26732027edd2c55c2ab4a77057bcfbba
romer8 <gio.busrom@gmail.com>
2021-11-10 12:32:14 -0700

    First prototype to finish data.

--- service_api/add_info.py
+++ service_api/add_info.py
@@ -6,9 +6,79 @@ import datetime
 import random
 import pandas as pd
 
+# To add Sites Make sure you have the following columns #
+
+    # SourceID,SiteName,SiteCode,Latitude,Longitude,
+    # SiteType,Elevation_m,Comments
+
+    # Example
+
+    # "SourceID": 35,
+    # "SiteName": "siteName",
+    # "SiteCode": "site1ID",
+    # "Latitude": 46.535,
+    # "Longitude": 15.2366,
+    # "SiteType": "Atmosphere",
+    # "Elevation_m": 256,
+    # "Comments": "site uploaded from Python"
+
+
+# To add Variables Make sure you have the following columns #
+
+    # VariableCode,VariableName,Speciation,VariableUnitsID,
+    # SampleMedium,ValueType,IsRegular,TimeSupport
+    # TimeUnitsID,DataType,GeneralCategory,NoDataValue
+
+    # Example
+
+    # "VariableCode": "var1ID",
+    # "VariableName": "Color",
+    # "Speciation": "Not Applicable",
+    # "VariableUnitsID": 189,
+    # "SampleMedium": "Groundwater",
+    # "ValueType": "Sample",
+    # "IsRegular": 1,
+    # "TimeSupport": 0,
+    # "TimeUnitsID": 100,
+    # "DataType": "Average",
+    # "GeneralCategory": "Hydrology",
+    # "NoDataValue": -9999
+
+# To add Sources Make sure you have the following columns #
+    # organization,descriptionlink,name, phone
+    # email,address,city,state,zipcode,citation,metadata
+
+    # Example
+
+    # "organization": "python source organization",
+    # "description": "uploaded from python:",
+    # "link": "http://example.com",
+    # "name": "Source name",
+    # "phone": "012-345-6789",
+    # "email": "test@gmail.com",
+    # "address": "any addres",
+    # "city": "any city",
+    # "state": "any state",
+    # "zipcode": "any zipcode",
+    # "citation": "uploaded from python as a test",
+    # "metadata": 10
+
+# To add DataValues Make sure you have the following columns #
+
+    # SiteID,VariableID,MethodID,SourceID,file_path
+
+    # "SiteID": "Any siteID",
+    # "VariableID": "Any variableID",
+    # "MethodID": "Any methodID",
+    # "SourceID": "Any sourceID",
+    # "file_path": "/path/to/file/containing/data/for/the/variable"
+
+    # The file from the file_path shuld have the date column called "dates"
+
+
+
 class HS:
     def addInformation(type_data,path_file,username,password):
-
         df = pd.read_csv(path_file,header=0)
         data_list = df.to_dict('records')
         for data in data_list:
