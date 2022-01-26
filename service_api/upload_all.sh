d49341f6fa14551d52bab8f5bf222c22fef6b108
romer8 <gio.busrom@gmail.com>
2022-01-26 12:36:39 -0700

    change to have miniconda and also reduced files

--- service_api/upload_all.sh
+++ service_api/upload_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source "${CONDA_HOME}"/bin/activate $3
+source ~/miniconda3/bin/activate $3
 
 echo "Uploading Sources . . ."
 
@@ -11,11 +11,12 @@ python add_info.py variables http://localhost/hydroserverlite/index.php/default/
 
 echo "Uploading Sites . . ."
 
-python add_info.py sites http://localhost/hydroserverlite/index.php/default/services/api ../data_test/Sites_Prueba.csv $1 $2
+python add_info.py sites http://localhost/hydroserverlite/index.php/default/services/api ../data_test/SitesPruebaReduced.csv $1 $2
 
 echo "Uploading Time Series . . ."
 
-python add_info.py values http://localhost/hydroserverlite/index.php/default/services/api ../data_test/dataValues.csv $1 $2
+python add_info.py values http://localhost/hydroserverlite/index.php/default/services/api ../data_test/dataValuesReduced.csv $1 $2
+
 echo "Deleting duplicates in series . . ."
 
 docker exec -i hs_d bash < ../delete_duplicates.sh
