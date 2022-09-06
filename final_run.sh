a7673599c9dd9a887184d64f28cb442d573b47a1
romer8 <gio.busrom@gmail.com>
2022-09-05 18:44:12 -0600

    added new configuration for the hydroserverlite

--- final_run.sh
+++ final_run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+echo "Sleeping to wait for mysql to start"
+sleep 10
 
-# echo 'Running the initial LAMP setup . . .'
-# /bin/bash run.sh
-# echo 'Finished . . .'
-echo 'Setting up mysql . . .'
+echo 'Setting up mysql database: Basically checking if the db and db user were setup. . .'
 /bin/bash db_config.sh
 echo 'Finished . . .'
-echo 'Installing HydroServerLite . . .'
+echo 'Installing HydroServerLite basically copying files etc. . .'
 /bin/bash hs_config.sh
 echo 'Finished . . .'
